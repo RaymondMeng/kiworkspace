cb5652dffe023855940b8404d4b9037da7a23ef7
RaymondMeng <2391530014@qq.com>
2023-11-04 21:44:14 +0800

    #2 fix error: 替换正则模式为首尾匹配，解决fp_name部分缺失问题

--- convert.py
+++ convert.py
@@ -2,15 +2,18 @@ from kiutils.board import Board
 from kiutils.utils import sexpr
 from kiutils.items.common import Net
 from kiutils.footprint import Footprint
+from kiutils.items.fpitems import *
 import re
+import sys
 
-#var
+#######*******vars*******#######
 net_name_list = [] 
 comp_ref_list = []
 comp_value_list = []
 comp_footprint_list = []
 comp_tstamps_list = []
-
+ 
+#######*******regular expression patterns*******#######
 # 使用正则表达式匹配 components、libparts 和 nets 的内容
 components_pattern = r'\(components(.*?)\(libparts'
 libparts_pattern = r'\(libparts(.*?)\(libraries'
@@ -29,10 +32,69 @@ net_pattern = r'\(net\s.*?\)\)\)'
 net_name_pattern = r'\(name\s".*?"\)' 
 net_node_pattern = r'\(node\s.*?\)\)' 
 
-information = "1.txt"
+######*******function*******#######
+def load_net(net_list, objectboard):
+    """
+    将指定.net文件中的net_name载入目标pcb文件
 
+    Args:
+    - net_list: .net文件中net的列表集合
+    - objectboard: kiutils库中Board类
+
+    Returns: None
+    """
+    lenth = len(net_list)
+    for i in range(lenth):
+        objectboard.nets.append(Net(number = i+1, name = net_list[i])) #因为是列表，值可以同步到外面参数引用
+
+
+
+def extract_string(text, start_symbol, end_symbol):
+    """
+    从字符串中提取两个字符中间的字符串
+
+    Args:
+    - text: 需要提取的字符串
+    - start_symbol: 开始字符
+    - end_symbol: 结束字符
+
+    Returns: 提取的字符串
+    """
+    pattern = re.compile(f"{start_symbol}(.*?){end_symbol}")
+    match = pattern.search(text)
+    if match:
+        return match.group(1)
+    return None
+
+
+#将footprint从kicad的封装库读取为footprint类，然后添加到pcb文件中
+def load_fp(fp_name_list, objectboard):
+    """
+    将指定.net文件中的footprint载入目标pcb文件
+
+    Args:
+    - fp_name_list: .net文件中footprint的列表集合
+    - objectboard: kiutils库中Board类
+
+    Returns: None
+    """
+    original_path = 'D:\\Kicad\\7.0\\share\\kicad\\footprints\\' #kicad封装库的地址
+    lenth = len(fp_name_list)
+    for i in range(lenth):
+        lib_name = extract_string(fp_name_list[i], '^', ':')
+        fp_name = extract_string(fp_name_list[i], ':', '$') #此处作分割，方便索引
+        fp_path = original_path + lib_name + '.pretty\\' + fp_name + '.kicad_mod' #封装的具体地址
+        print(fp_path)
+        fp_origin = Footprint.from_file(filepath = fp_path)
+        objectboard.footprints.append(fp_origin)
+        objectboard.footprints[i].libraryNickname = lib_name #kicad_mod文件中不含libnickname，需要额外添加
+        objectboard.footprints[i].graphicItems[0].text = comp_ref_list[i] #修复ref_name没有命名的问题
+
+
+
+#######*******regular expression*******#######
 # 读取并分割文件为行
-net_file_contents = open("C:\\Users\\23915\\Desktop\\eda\\demo.net", "r").read()
+net_file_contents = open("E:\\sch-convert-pcd\\demo.net", "r").read()
 
 components_match = re.search(components_pattern, net_file_contents, re.DOTALL)
 libparts_match = re.search(libparts_pattern, net_file_contents, re.DOTALL)
@@ -58,27 +120,27 @@ for net in net_match:
 
 
 #components主要解析 ref、footprint、value、tstamps
+#修改一下正则模式，改为首尾匹配，避免字母被截取
 for comp in comp_match:
     #comp_ref解析
     comp_ref_match = re.search(comp_ref_pattern, comp, re.DOTALL)
     comp_ref = comp_ref_match.group(0).strip() if comp_ref_match else "Not found"
-    comp_ref_list.append(comp_ref.strip('(ref "")'))
+    comp_ref_list.append(extract_string(comp_ref, 'ref "', '"'))
 
     #comp_value解析
     comp_value_match = re.search(comp_value_pattern, comp, re.DOTALL)
     comp_value = comp_value_match.group(0).strip() if comp_value_match else "Not found"
-    comp_value_list.append(comp_value.strip('(value "")'))
+    comp_value_list.append(extract_string(comp_value, 'value "', '"'))
 
     #comp_footprint解析
     comp_footprint_match = re.search(comp_footprint_pattern, comp, re.DOTALL)
     comp_footprint = comp_footprint_match.group(0).strip() if comp_footprint_match else "Not found"
-    comp_footprint_list.append(comp_footprint.strip('(footprint "")'))
-    #print(comp_footprint)
-
+    comp_footprint_list.append(extract_string(comp_footprint, 'footprint "', '"'))
+    
     #comp_tstamps解析
     comp_tstamps_match = re.search(comp_tstamp_pattern, comp, re.DOTALL)
     comp_tstamps = comp_tstamps_match.group(0).strip() if comp_tstamps_match else "Not found"
-    comp_tstamps_list.append(comp_tstamps.strip('(tstamps "")'))
+    comp_tstamps_list.append(extract_string(comp_tstamps, 'tstamps "', '"'))
 
 #print(comp_tstamps_list)
 
@@ -98,40 +160,13 @@ print(comp_tstamps_list)
 print("\nnet_name_list:")
 print(net_name_list)
 
-"""
 
-cgc's part
-
-"""
-
-#将指定.net文件中的net_name载入目标pcb文件
-def load_net(net_list, objectboard):
-    lenth = len(net_list)
-    for i in range(lenth):
-        objectboard.nets.append(Net(number = i+1, name = net_list[i]))
-
-#提取两个字符中间的字符串
-def extract_string(text, start_symbol, end_symbol):
-    pattern = re.compile(f"{start_symbol}(.*?){end_symbol}")
-    match = pattern.search(text)
-    if match:
-        return match.group(1)
-    return None
-
-#将footprint从kicad的封装库读取为footprint类，然后添加到pcb文件中
-def load_fp(fp_name_list, objectboard):
-    original_path = 'F:\\KiCad\\7.0\\share\\kicad\\footprints\\' #kicad封装库的地址
-    lenth = len(fp_name_list)
-    for i in range(lenth):
-        lib_name = extract_string(fp_name_list[i], '^', ':')
-        fp_name = extract_string(fp_name_list[i], ':', '$') #此处作分割，方便索引
-        fp_path = original_path + lib_name + '.pretty\\' + fp_name + '.kicad_mod' #封装的具体地址
-        fp_origin = Footprint.from_file(filepath = fp_path)
-        objectboard.footprints.append(fp_origin)
-        objectboard.footprints[i].libraryNickname = lib_name #kicad_mod文件中不含libnickname，需要额外添加
-
-if __main__ == "__main__":
+def main():
     board = Board.create_new()
-    #load_fp(comp_footprint_list, board)
+    load_fp(comp_footprint_list, board)
     load_net(net_name_list, board)
-    board.to_file('C:\\Users\\2391\\Desktop\\eda\\kiworkspace\\test.kicad_pcb')
+    board.to_file('E:\\sch-convert-pcd\\test.kicad_pcb')
+    sys.exit()
+
+if __name__ == "__main__":
+    main()
